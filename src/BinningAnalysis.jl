247e1fe26e2475b4c5f79e79bd17c95cf8f2d1e3
Carsten Bauer <crstnbr@gmail.com>
2019-01-28 18:51:23 +0100

    do not export `all_means`; closes #4

--- src/BinningAnalysis.jl
+++ src/BinningAnalysis.jl
@@ -8,7 +8,7 @@ export LogBinner, push!, append!
 
 include("statistics.jl")
 export mean, var, varN, tau, std_error
-export all_means, all_vars, all_varNs, all_taus, all_std_errors
+export all_vars, all_varNs, all_taus, all_std_errors
 export convergence, has_converged
 
 include("cosmetics.jl")
